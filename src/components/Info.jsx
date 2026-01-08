40ee5efcf4f665d50091c19a2023395df7f0c57d
Larbi <larbi.louad.el@gmail.com>
2026-01-08 18:37:45 +0100

    package  vercel

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,12 +3,12 @@ import { personalInfo } from "../Data"
 const Info = () => {
     return (
         <>
-            {personalInfo.map(({ title, description,skills }, index) => {
+            {personalInfo.map(({ title, description, skills }, index) => {
                 return (
                     <li className="info-items" key={index}>
                         <span className="info-title"> {title}</span>
                         <span className="info-description"> {description}</span>
-                       
+
                     </li>
                 )
             })}
